6208e7da8f6742c2e3171f81c9ac649724b59b1c
stevenlandis <stevenlandis@comcast.net>
2022-03-13 14:50:23 -0700

    add js to fetch count message

--- site-js/src/index.tsx
+++ site-js/src/index.tsx
@@ -1,6 +1,15 @@
+import { createSignal } from "solid-js";
 import { render } from "solid-js/web";
 
 function App() {
+  const [message, setMessage] = createSignal<string>();
+
+  (async () => {
+    const resp = await fetch("/", { method: "POST" });
+    const text = await resp.text();
+    setMessage(text);
+  })();
+
   return (
     <div>
       <h1>A cool webserver</h1>
@@ -8,6 +17,7 @@ function App() {
         This server is homegrown and runs on a kindle fire. It's written in 100%
         Go so it should be pretty fast.
       </div>
+      <div>{message() !== undefined && message()}</div>
     </div>
   );
 }
